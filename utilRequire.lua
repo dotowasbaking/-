a7837df29eb106ccbd63eb5903a893b360163825
dotowasbaking <70781219+dotowasbaking@users.noreply.github.com>
2022-07-07 23:18:05 -0400

    Revert "Update utilRequire.lua"
    This reverts commit d35e766187fddacd3c07269fe9eb12aaad4c577b.

--- utilRequire.lua
+++ utilRequire.lua
@@ -1,6 +1,6 @@
 return function(fileName)
     return loadstring(syn.request({
-        Url = ("https://raw.githubusercontent.com/dotowasbaking/-/ab0a0dbbae1568d4440c9f03c630de47882b11e1/%s.lua"):format(fileName); -- HACK :SCREAM:
+        Url = ("https://raw.githubusercontent.com/dotowasbaking/-/main/%s.lua"):format(fileName);
         Method = "GET";
     }).Body)() -- no error handling cus if u fuck this up ur speed
 end
